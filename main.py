cc9289c89065de534028033bb1c541db27549267
liyechen <gliyechen@hotmail.com>
2019-04-02 19:00:31 +0800

    chore

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import base64
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+def login_post(data_login):
+    login_url = "https://h5.ele.me/restapi/eus/login/mobile_send_code"
+    r_login = requests.post(login_url, data = data_login)
+    return r_login
+
 def eleme_login():
     print('Eleme account:'),
     eleme_acc = str(input())
     # urls
-    login_url = "https://h5.ele.me/restapi/eus/login/mobile_send_code"
     get_captcha = "https://h5.ele.me/restapi/eus/v3/captchas"
-    r_login = requests.post(login_url, data = {'captcha_hash': '', 'captcha_hash': '', 'mobile': eleme_acc, 'scf': 'ms'})
+    r_login = login_post({'captcha_hash': '', 'captcha_hash': '', 'mobile': eleme_acc, 'scf': 'ms'})
     if r_login.status_code == 200:
         print 'success'
     else:
@@ -21,6 +25,7 @@ def eleme_login():
             captcha_img = captcha_resp['captcha_image']
             # get image data from base64 string 
             image_data = base64.b64decode(captcha_img[22:])
+            captcha_hash = captcha_resp['captcha_hash']
             captcha_file_name = 'captcha_img.jpg'
             with open(captcha_file_name, 'wb') as f:
                 f.write(image_data)
@@ -28,7 +33,7 @@ def eleme_login():
             imgplot = plt.imshow(img)
             plt.show()
             print ('Captcha:'),
-            captcha_str = input()
+            captcha_str = str(input())
             
 
 
